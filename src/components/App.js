a4d1b0468392efd2a947aae50c21dbef18f3d129
Aleksandr Kulikov <aleksandr-kulikov-13@mail.ru>
2023-06-17 15:05:01 +0300

    feat: added function App, added page.css

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,12 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="page">
+      <Header />
+    </div>
+  );
+}
+export default App;
