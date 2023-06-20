0fbb28719279b94be88653f2d9911d2a979a28ed
Aleksandr Kulikov <aleksandr-kulikov-13@mail.ru>
2023-06-20 23:32:09 +0300

    fix: change div on li

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import logo from "../images/logo.png";
 
@@ -5,11 +6,12 @@ function Header() {
   return (
     <div className="header">
       <img className="header__logo" src={logo} alt="" />
-      <div className="header__nav">
-        <button className="header__nav-links">Контакты</button>
-        <button className="header__nav-links">Акции</button>
-        <button className="header__nav-links">Как нас найти</button>
-      </div>
+      <ul className="header__nav">
+        <li className="header__nav-links">Контакты</li>
+        <li className="header__nav-links">Акции</li>
+        <li className="header__nav-links">Как нас найти</li>
+        //!todo посмотреть как можно сделать список красиво. эффект курсора должен работать только на слове
+      </ul>
       <div className="header__contacts">
         <button className="header__contacts-vk" />
         <button className="header__contacts-whatsup" />
