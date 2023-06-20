50c97713e0f3b3e4e7bec566522056c6383e89e3
Aleksandr Kulikov <aleksandr-kulikov-13@mail.ru>
2023-06-21 00:06:09 +0300

    fix: header buttons is active opacity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,6 @@ function Header() {
         <li className="header__nav-links">Контакты</li>
         <li className="header__nav-links">Акции</li>
         <li className="header__nav-links">Как нас найти</li>
-        //!todo посмотреть как можно сделать список красиво. эффект курсора должен работать только на слове
       </ul>
       <div className="header__contacts">
         <button className="header__contacts-vk" />
