8a0523e0fad968dda4fb36cf89f5ec60c59f71fb
Aleksandr <madness281@gmail.com>
2023-09-17 18:19:46 +0300

    install npm yap ,popups close on musedown

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import HandleCard from "../utils/HandleCard";
 import PopupWithConatcs from "./PopupWithContacts";
 import PopupWithPromo from "./PopupWithPromo";
 import PopupWithMap from "./PopupWithMap";
+import { useEffect } from "react";
 
 function App() {
   const [isShowContactsPopupOpen, setIsShowContactsPopupOpen] = useState(false);
@@ -15,9 +16,11 @@ function App() {
   function handleShowContacsClick() {
     setIsShowContactsPopupOpen(true);
   }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   function handleShowPromoClick() {
     setIsShowPromoPopupOpen(true);
   }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   function handleShowMapClick() {
     setIsShowMapPopupOpen(true);
   }
@@ -27,6 +30,12 @@ function App() {
     setIsShowPromoPopupOpen(false);
     setIsShowMapPopupOpen(false);
   }
+  useEffect(() => {
+    document.body.addEventListener("mousedown", closeAllPopup);
+    return () => {
+      document.body.removeEventListener("mousedown", closeAllPopup);
+    };
+  }, [setIsShowContactsPopupOpen, handleShowPromoClick, handleShowMapClick]);
 
   return (
     <div className="page">
