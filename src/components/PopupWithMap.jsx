8071502da7897e8ae276d62d61d594e973a523a7
Aleksandr <madness281@gmail.com>
2023-12-04 13:40:20 +0300

    add working link to the map in PopupWithMap

--- src/components/PopupWithMap.jsx
+++ src/components/PopupWithMap.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
 import PopupWithoutForm from "./PopupWithoutForm";
+import mapIcon from "../images/yandex-maps-svgrepo-com.svg";
 
 function PopupWithMap({ isOpen, onClose, info }) {
   return (
@@ -10,14 +11,23 @@ function PopupWithMap({ isOpen, onClose, info }) {
       onClose={onClose}
       info={
         <>
-          <span>
-            Пр-т Юрия Гагарина 34 <br />
-            Территория "Южный Рынок" (Павильон К-37)
-          </span>
-          <iframe
-            className="map"
-            src="https://yandex.ru/map-widget/v1/?um=constructor%3A1bb9e61aa99c40a31966a8bce632072d61bfdfaa3a837ff462309e2c0f26e094&amp;source=constructor"
-          ></iframe>
+          <>
+            <span>
+              Пр-т Юрия Гагарина 34 <br />
+              Территория "Южный Рынок" (Павильон К-37)
+            </span>
+            <iframe
+              className="map"
+              src="https://yandex.ru/map-widget/v1/?um=constructor%3A1bb9e61aa99c40a31966a8bce632072d61bfdfaa3a837ff462309e2c0f26e094&amp;source=constructor"
+            ></iframe>
+          </>
+          <a
+            className="map__link"
+            href="https://yandex.ru/maps/?from=mapframe&l=trf%2Ctrfe&ll=30.341919%2C59.847249&source=mapframe&um=constructor%3A1bb9e61aa99c40a31966a8bce632072d61bfdfaa3a837ff462309e2c0f26e094&utm_source=mapframe&z=15.6"
+          >
+            <img className="map__icon" src={mapIcon} alt="" /> Перейти на
+            большую карту
+          </a>
         </>
       }
     />
