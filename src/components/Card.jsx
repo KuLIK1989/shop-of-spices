a0abd74d08802427b7f34863697f96ef3574a4d1
Aleksandr <madness281@gmail.com>
2023-12-14 19:06:27 +0300

    переделан компонент card есть возможность набирать количество специй

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+// import cart from "../images/cart-shopping-svgrepo-com.svg";
 
 const Card = ({ props }) => {
+  const [cartSum, setCartSum] = useState(0);
+  const onClickCartPlus = () => {
+    setCartSum(cartSum + props.price);
+  };
+  const onClickCartMinus = () => {
+    setCartSum(cartSum - props.price);
+  };
+
   return (
+    // передалать по классы по БЭМ
     <div className="card">
       <img className="card__image" src={props.link} alt={props.name} />
-      <h2 className="card__name">{props.name}</h2>
-      <p className="card__price">100г\{props.price} &#8381;</p>
-      <button className="card__btn">В корзину</button>
+      <h2 className="card__name">{props.name} 100г</h2>
+      <div className="card__buy">
+        <div className="card__weight">
+          <button
+            onClick={onClickCartMinus}
+            className="card__button-minus"
+          ></button>
+          <input
+            className="card__input"
+            type="text"
+            value={props.price}
+          />
+          <button
+            onClick={onClickCartPlus}
+            className="card__button-plus"
+          ></button>
+        </div>
+        <button className="card__btn">В корзину</button>
+        <div className="card__sum">{cartSum}₽</div>
+      </div>
     </div>
   );
 };
