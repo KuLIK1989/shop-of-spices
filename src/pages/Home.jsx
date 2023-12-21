3503fa1fdbba9a1f7f6518a5a1d395dd472c7404
Aleksandr <madness281@gmail.com>
2023-12-21 13:56:23 +0300

    установлены redux axios. метод fetch заменен axios get

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,17 @@ import { useState } from "react";
 import { useEffect } from "react";
 import Skeleton from "../components/SkeletonReact";
 import Card from "../components/Card";
+import axios from "axios";
 
 function Home() {
   const [items, setItems] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://657c576c853beeefdb993d3c.mockapi.io/items")
+    axios
+      .get("https://657c576c853beeefdb993d3c.mockapi.io/items")
       .then((res) => {
-        return res.json();
-      })
-      .then((json) => {
-        setItems(json);
+        setItems(res.data);
         setIsLoading(false);
       });
   }, []);
@@ -23,11 +22,9 @@ function Home() {
       <section className="cards">
         {isLoading
           ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => <Card key={obj.id} {...obj}   />)}
-          
+          : items.map((obj) => <Card key={obj.id} {...obj} />)}
       </section>
     </>
   );
-  
 }
 export default Home;
