b0681ff2a04b989790808b516bb02bdbb301b5f2
Aleksandr <madness281@gmail.com>
2023-12-26 12:28:36 +0300

    test

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -10,44 +10,47 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addItem(state, action) {
-      console.log("initialState-AddItem", initialState);
-      const findItem = state.items.find((obj) => obj.id === action.payload.id);
-      console.log("findItem в добавлении", findItem);
-      if (findItem) {
-      } else {
-        state.items.push(action.payload);
-      }
+      // console.log("initialState-AddItem", initialState);
+      // const findItem = state.items.find((obj) => obj.id === action.payload.id);
+      // console.log("findItem в добавлении", findItem);
+      // if (findItem) {
+      // } else {
+      //   state.items.push(action.payload);
+      // }
 
-      state.totalPrice = state.items.reduce((sum, obj) => {
-        return obj.price * obj.count + sum;
-      }, 0);
+      // state.totalPrice = state.items.reduce((sum, obj) => {
+      //   return obj.price * obj.count + sum;
+      // }, 0);
+      const findItem = state.items.find((obj) => obj.id === action.payload.id);
+      console.log(findItem);
+      
     },
     minusItem(state, action) {
-      const findItem = state.items.find((obj) => obj.id === action.payload);
-      console.log(findItem);
-      if (findItem.count !== 0) {
-        findItem.count--;
-      }
-      state.totalPrice = state.items.reduce((sum, obj) => {
-        return obj.price * obj.count - sum;
-      }, 0);
+      // const findItem = state.items.find((obj) => obj.id === action.payload);
+      // console.log(findItem);
+      // if (findItem.count !== 0) {
+      //   findItem.count--;
+      // }
+      // state.totalPrice = state.items.reduce((sum, obj) => {
+      //   return obj.price * obj.count - sum;
+      // }, 0);
     },
     plusItem(state, action) {
-      console.log("initialState-itemsPlusItem", initialState);
-      const findItem = state.items.find((obj) => obj.id === action.payload.id);
-      console.log("plusItem", findItem);
-      if (findItem) {
-        findItem.count++;
-        console.log("сработало добавление");
-      } else {
-        state.items.push({
-          ...action.payload,
-          count: 1,
-        });
-      }
-      state.totalPrice = state.items.reduce((sum, obj) => {
-        return obj.price * obj.count + sum;
-      }, 0);
+      // console.log("initialState-itemsPlusItem", initialState);
+      // const findItem = state.items.find((obj) => obj.id === action.payload.id);
+      // console.log("plusItem", findItem);
+      // if (findItem) {
+      //   findItem.count++;
+      //   console.log("сработало добавление");
+      // } else {
+      //   state.items.push({
+      //     ...action.payload,
+      //     count: 1,
+      //   });
+      // }
+      // state.totalPrice = state.items.reduce((sum, obj) => {
+      //   return obj.price * obj.count + sum;
+      // }, 0);
     },
     removeItem(state, action) {
       state.items = state.items.filter((obj) => obj.id !== action.payload);
