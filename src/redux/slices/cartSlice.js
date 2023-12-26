dc8d08ae60fb9fd1a6e641c8484b4cebc2fda0e1
Aleksandr <madness281@gmail.com>
2023-12-26 21:16:18 +0300

    рефакторнг

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -12,16 +12,6 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addItem(state, action) {
-      // console.log("initialState-AddItem", initialState);
-      // const findItem = state.items.find((obj) => obj.id === action.payload.id);
-      // console.log("findItem в добавлении", findItem);
-      // if (findItem) {
-      // } else {
-      //   state.items.push(action.payload);
-      // }
-      // state.totalPrice = state.items.reduce((sum, obj) => {
-      //   return obj.price * obj.count + sum;
-      // }, 0);
       const findItem = state.finalItems.find(
         (obj) => obj.id === action.payload.id
       );
@@ -33,33 +23,31 @@ export const cartSlice = createSlice({
       }, 0);
     },
     minusItem(state, action) {
-      // const findItem = state.items.find((obj) => obj.id === action.payload);
-      // console.log(findItem);
-      // if (findItem.count !== 0) {
-      //   findItem.count--;
-      // }
-      // state.totalPrice = state.items.reduce((sum, obj) => {
-      //   return obj.price * obj.count - sum;
-      // }, 0);
+      const findItem = state.items.find((obj) => obj.id === action.payload);
+      const superPlusItem = state.superFinalItems.find(
+        (obj) => obj.id === action.payload
+      );
+      if (superPlusItem.count !== 0) {
+        superPlusItem.count--;
+      }
+
+      console.log(findItem);
+      if (findItem.count !== 0) {
+        findItem.count--;
+      }
+      state.totalPrice = state.superFinalItems.reduce((sum, obj) => {
+        return obj.price * obj.count - sum;
+      }, 0);
     },
     plusItem(state, action) {
       //функция не увеличивает количество и стоимость без нажатия кнопки "в корзину" даже если нажимаю кнопку + в корзине
-
-      // console.log("initialState-itemsPlusItem", initialState);
-      // const findItem = state.items.find((obj) => obj.id === action.payload.id);
-      // console.log("plusItem", findItem);
-      // if (findItem) {
-      //   findItem.count++;
-      //   console.log("сработало добавление");
-      // } else {
-      //   state.items.push({
-      //     ...action.payload,
-      //     count: 1,
-      //   });
-      // }
-      // state.totalPrice = state.items.reduce((sum, obj) => {
-      //   return obj.price * obj.count + sum;
-      // }, 0);
+      const superPlusItem = state.superFinalItems.find(
+        (obj) => obj.id === action.payload.id
+      );
+      if (superPlusItem) {
+        superPlusItem.count++;
+        console.log("superPlusItem", superPlusItem.count);
+      }
       const findItem = state.items.find((obj) => obj.id === action.payload.id);
       console.log("plusItem findItem", findItem);
       if (findItem) {
@@ -69,6 +57,9 @@ export const cartSlice = createSlice({
         state.items.push({ ...action.payload, count: 1 });
         console.log("PLuseItem else", state.finalItems);
       }
+      state.totalPrice = state.superFinalItems.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
       state.finalItems = state.items;
       console.log("что-то прошло в finalItems", state.finalItems);
     },
